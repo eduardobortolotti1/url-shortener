bda408c567afb5a76534d6f76894aad7b19349ff
eduardobortolotti1 <tiolollipop@gmail.com>
2024-07-03 19:33:47 -0300

    fix: Fixed setup file

--- back-end-api/setup/setup.js
+++ back-end-api/setup/setup.js
@@ -1,39 +1,45 @@
 import { configDotenv } from 'dotenv';
-import pkg from 'pg';
-const { Pool } = pkg;
+import pg from 'pg';
+const { Pool } = pg;
 
 configDotenv({ path: "./.env" })
 
 console.log(process.env.DB_USER);
 
-const pool = new Pool({
+const pool_postgres = new Pool({
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,
 	database: process.env.DB_DATABASE,
 	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT || 5432, // default PostgreSQL port
+	port: process.env.DB_PORT || 5432,
 });
 
-const client = await pool.connect();
+const pool_url_shortener = new Pool({
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_CREATED_DATABASE,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT || 5432,
+});
 
 async function createDatabase() {
 	try {
-		await client.query('CREATE DATABASE "url-shortener"');
+		// Creating new Database
+		await pool_postgres.query('CREATE DATABASE "url-shortener"');
 		const createTableQuery = `
-			CREATE TABLE "urls" (
+			CREATE TABLE urls (
 			id SERIAL PRIMARY KEY,
 			original_url TEXT NOT NULL,
 			shortened_url TEXT NOT NULL,
 			creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 			)
-			`;
-		await client.query(createTableQuery);
+		`;
 
-		client.release();
+		// Creating the table inside the new database
+		await pool_url_shortener.query(createTableQuery);
 		console.log("Project setup successfully!");
 	} catch (error) {
 		console.error("Error during setup:", error);
-		client.release();
 	}
 }
 createDatabase()
